6c268f7ce383dd35fe41948e3969fdf89d0450fa
Brian Rehrey <b.rehrey@gmail.com>
2015-12-05 16:44:15 +0000

    Implemented the card set for discussion. Will be added to the initialize methods of player and solution set and will be held and created in the game class

---
+++ lib/card_set.rb
@@ -0,0 +1,49 @@
+
+
+class CardSet
+
+  attr_accessor :available_cards 
+  
+  def initialize
+    puts "Creating Card Set"
+    
+    available_weapons = ["Candlestick", "Poison", "Rope", "Gloves", "Horseshoe", 
+                         "Knife"," Lead Pipe"]
+                         
+    available_suspects = ["Ms. Vivienne Scarlet", "Col. Michael Mustard", 
+                          "Mrs. Blanche White", "Rev. Jonathan Green", 
+                          "Mrs. Elizabeth Peacock", "Prof. Peter Plum"]
+                          
+    available_rooms = ["Kitchen", "Ballroom", "Conservatory", "Billiard Room", 
+                       "Library", "Study", "Hall", "Lounge", "Dining Room", 
+                       "Cellar"]
+    
+    
+    #Initialize a map of each card type.
+    @available_cards = [{:weapons => available_weapons}, 
+                        {:suspects => available_suspects}, 
+                        {:rooms => available_rooms}] 
+    
+  end
+  
+  #Retrieve a random weapon card and remove it from the weapon map
+  def getRandomWeaponCard
+    card = available_cards["weapons"].sample
+    available_cards["weapons"].delete(card)
+    return card
+  end
+  
+  #Retrieve a random suspect card and remove it from the weapon map
+  def getRandomSuspectCard
+    card = available_cards["suspects"].sample
+    available_cards["suspects"].delete(card)
+    return card
+  end
+  
+  #Retrieve a random room card and remove it from the weapon map
+  def getRandomRoomCard
+    card = available_cards["rooms"].sample
+    available_cards["rooms"].delete(card)
+    return card
+  end
+end
