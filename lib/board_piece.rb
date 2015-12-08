289bb3628adc4a58870169c6e9a69cbc491bef3a
Billy Yarosh <yaroshw1@gmail.com>
2015-12-07 20:28:05 -0500

    player now has game piece

--- lib/board_piece.rb
+++ lib/board_piece.rb
@@ -3,8 +3,8 @@ require 'card'
 class BoardPiece < Card
   attr_accessor :character_name
 
-  def initialize(character_id, character_name)
+  def initialize(character_id, character_name = nil)
     super(character_id, CardType::SUSPECT)
-    self.character_name = character_name
+    self.character_name = character_name || character_id
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,18 +1,20 @@
 require 'board_actions'
+require 'board_piece'
 
 class Player < BoardAction
-
   attr_accessor :name
   attr_accessor :cards
   attr_accessor :id
   attr_accessor :disabled
+  attr_accessor :board_piece
 
-  def initialize(name, id, cardset)
+  def initialize(name, id, cardset, character_name)
     @name = name
     @id = id
     @disabled = false;
     @cards = {:weapon => cardset.random_weapon_card,
               :suspect => cardset.random_suspect_card,
               :room => cardset.random_room_card}
+    @board_piece = BoardPiece.new(character_name)
   end
 end
