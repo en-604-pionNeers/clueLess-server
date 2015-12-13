a69e6cd9e479eb19396777c7161fefa52a6b052a
Brian Rehrey <b.rehrey@gmail.com>
2015-12-13 17:29:18 +0000

    Changed the cards to use generic item_name variable to make it easier on client side

--- lib/board_piece.rb
+++ lib/board_piece.rb
@@ -1,10 +1,10 @@
 require 'card'
 
 class BoardPiece < Card
-  attr_accessor :character_name
+  attr_accessor :item_name 
 
   def initialize(character_id, character_name = nil)
     super(character_id, CardType::SUSPECT)
-    self.character_name = character_name || character_id
+    self.item_name  = character_name || character_id
   end
 end
